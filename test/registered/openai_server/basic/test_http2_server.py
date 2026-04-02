51db219a84d8cf52f25448562b8f16249f5d9a66
ispobock <ispobaoke@gmail.com>
2026-04-02 10:43:02 +0000

    update

--- test/registered/openai_server/basic/test_http2_server.py
+++ test/registered/openai_server/basic/test_http2_server.py
@@ -27,7 +27,7 @@ try:
 except ImportError:
     _HAS_GRANIAN = False
 
-register_cuda_ci(est_time=120, suite="stage-b-test-small-1-gpu")
+register_cuda_ci(est_time=120, suite="stage-b-test-1-gpu-small")
 
 
 @unittest.skipUnless(_HAS_GRANIAN, "granian not installed (pip install sglang[http2])")
