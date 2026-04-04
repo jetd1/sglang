047ad94efc277ea850700355af9f3b8c0a13a686
Mick <mickjagger19@icloud.com>
2026-04-04 19:16:02 +0800

    upd

--- python/sglang/multimodal_gen/configs/models/dits/flux.py
+++ python/sglang/multimodal_gen/configs/models/dits/flux.py
@@ -18,7 +18,7 @@ class FluxArchConfig(DiTArchConfig):
     num_attention_heads: int = 24
     joint_attention_dim: int = 4096
     pooled_projection_dim: int = 768
-    guidance_embeds: bool = True
+    guidance_embeds: bool = False
     axes_dims_rope: Tuple[int, int, int] = (16, 56, 56)
 
     stacked_params_mapping: list[tuple[str, str, str]] = field(default_factory=list)
